a3f649e7a532418d0956b6126ce016d0f396a4d2
Edgar Abraham Gutierrez Venegas <abraham_venegaz@hotmail.com>
2026-07-27 17:41:47 -0700

    chore: remove diagnostic script and restore production error handling

--- config/config.php
+++ config/config.php
@@ -86,7 +86,7 @@ define('EMAIL_FROM_NAME', SITE_NAME);
 define('EMAIL_REPLY_TO', BOOKING_EMAIL);
 
 // Modo debug (cambiar a false en producción)
-define('DEBUG_MODE', true);
+define('DEBUG_MODE', false);
 if (DEBUG_MODE) {
 
     ini_set('display_errors', 1);
