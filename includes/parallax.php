8eb5bee9b69dcb6e5f571ecf8001740227f4c02e
Edgar Abraham Gutierrez Venegas <abraham_venegaz@hotmail.com>
2026-07-29 23:00:17 -0700

    style: increase parallax section heiht

--- includes/parallax.php
+++ includes/parallax.php
@@ -1,4 +1,4 @@
-<section class="relative h-[780px] max-md:h-[360px] bg-fixed max-md:bg-scroll bg-cover bg-center flex items-center justify-center"
+<section class="relative h-[900px] max-md:h-[500px] bg-fixed max-md:bg-scroll bg-cover bg-center flex items-center justify-center"
          style="background-image: url('/assets/media/adventure.jpg');">
   <div class="absolute inset-0 bg-gradient-to-br from-navy/75 to-navy-dark/60"></div>
   <div class="relative text-center px-8 py-10 max-w-lg bg-white/10 backdrop-blur-sm rounded-2xl">
